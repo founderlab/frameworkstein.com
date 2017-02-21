270a9e176e8992d2cf76b9c15976d3ec5d83bc37
gwilym <gwilym@gwilymh.com>
2017-02-22 10:51:14 +1100

    Add quickstart page

--- shared/modules/pages/components/QuickStart.js
+++ shared/modules/pages/components/QuickStart.js
@@ -27,63 +27,67 @@ export default class Packages extends Component {
                 You'll need <a href="https://nodejs.org">Node.js</a>, <a href="https://www.postgresql.org">PostgreSQL</a> and <a href="https://redis.io/">Redis</a>.
               </p>
               <p>
-                First up, you need to make sure you have a few programs installed. For brevity's sake we're going to assume you're on a mac for this step - hit up their respective homepages for installion instructions on other systems.
+                First up, you need to make sure you have a few programs installed. For brevity's sake we're going to assume you're on a mac for this step - hit up their respective homepages for installation instructions on other systems.
               </p>
+              <pre>
+                $ brew install postgresql{'\n'}
+                $ brew install redis{'\n'}
+                $ brew install node{'\n'}
+              </pre>
 
               <p>
-                <pre>
-                  $ brew install postgresql{'\n'}
-                  $ brew install redis{'\n'}
-                  $ brew install node{'\n'}
-                </pre>
+                Make sure that Postgres and Redis are running before you proceed. Easiest way is to use <a href="https://github.com/Homebrew/homebrew-services">Homebrew Services</a>, which will ensure they also start at startup.
               </p>
-              <p>
-                Make sure that they're running before you proceed.
-              </p>
+              <pre>
+                $ brew tap homebrew/services{'\n'}
+                $ brew services start postgresql{'\n'}
+                $ brew services start redis{'\n'}
+              </pre>
 
               <h2 className="subhead">
-                2. Install the Frameworkstein cli
+                2. Install the Frameworkstein cli and a few global dependencies
               </h2>
-              <p>
-                <pre>
-                  $ npm i frameworkstein -g{'\n'}
-                </pre>
+              <p className="small">
+                <a href="https://babeljs.io">Babel</a> & <a href="https://webpack.github.io">webpack</a> are used to transpile JavaScript. <a href="https://nodemon.io">Nodemon</a> is used to auto restart the node server when files change.
               </p>
+              <pre>
+                $ npm install -g frameworkstein nodemon babel@5 webpack{'\n'}
+              </pre>
 
               <h2 className="subhead">
                 3. Create a new app
               </h2>
-              <p>
-                <pre>
-                  $ stein new myapp{'\n'}
-                  $ cd myapp
-                </pre>
-              </p>
+              <pre>
+                $ stein new myapp{'\n'}
+                $ cd myapp{'\n'}
+                $ npm install{'\n'}
+              </pre>
 
               <h2 className="subhead">
                 4. Run the app
               </h2>
               <p className="small">
-                There's two commands to run in parallel here - one for the node server and one for webpack.
+                There's two commands to run in parallel here - one for the node server and one for webpack. You'll need two terminals open to run them both at the same time.
               </p>
-              <p>
-                <pre>
-                  # Run the node server (with a development environment){'\n'}
-                  $ npm run start-dev
-                </pre>
-              </p>
-              <p>
-                <pre>
-                  # Run the webpack server in another terminal{'\n'}
-                  $ npm run client-watch
-                </pre>
-              </p>
+              <pre>
+                # Run the node server (with a development environment){'\n'}
+                $ npm run start-dev
+              </pre>
+              <pre>
+                # Run the webpack server in another terminal{'\n'}
+                $ npm run client-watch
+              </pre>
+
+              <p>You should have something looking like this:</p>
+              <p><a href="/public/images/quickstart.jpg" target="_blank"><img src="/public/images/quickstart.jpg" /></a></p>
 
               <h2 className="subhead">
-                4. That's it! <a href="http://localhost:3000">Visit your monster of a new project</a>
+                4. Nice one! We've created a monster
               </h2>
               <p>
                 If all went well your app is running and can be found at <a href="http://localhost:3000">http://localhost:3000</a>.
+              </p>
+              <p>
                 Frameworkstein will have created a database for you and populated it with some dummy data.
               </p>
 
@@ -91,20 +95,28 @@ export default class Packages extends Component {
                 5. Add a model
               </h2>
               <p className="small">(extra credit)</p>
-              <p>
-                <pre>
-                  $ stein add-model Monster
-                </pre>
-              </p>
-              <p>You now have a Monster model with REST endpoints at <a href="http://localhost:3000/api/monsters">http://localhost:3000/api/monsters</a></p>
+              <pre>
+                # Stop the server with `ctrl-c`{'\n'}
+                $ stein add-model Monster{'\n'}
+                # Run the server again{'\n'}
+                $ npm run start-dev{'\n'}
+              </pre>
+              <p>You now have a Monster model with REST endpoints at <a href="http://localhost:3000/api/monsters">http://localhost:3000/api/monsters</a>.</p>
 
               <h2 className="subhead">
                 6. Edit your models
               </h2>
               <p className="small">(and a gold star)</p>
+              <p>Open up <code>shared/configureAdmin.js</code> and add an entry like the following to the models array:</p>
+              <pre>
+{`{
+  Model: require('./models/Monster'),
+},`}
+              </pre>
+              <p>Give the model some data fields in its schema at <code>shared/models/schemas/monster.js</code></p>
               <p>Visit the admin panel at <a href="http://localhost:3000/admin/monsters">http://localhost:3000/admin/monsters</a> to do CRUD things to your Monsters.</p>
-              <p>Login email: <code>admin@frameworkstein.com</code></p>
-              <p>Password: <code>frameworkstein</code></p>
+              <p className="small">Login email: <code>admin@frameworkstein.com</code></p>
+              <p className="small">Password: <code>frameworkstein</code></p>
               <p className="small">You can modify these defaults in the <code>scaffold</code> directory.</p>
 
             </Col>
